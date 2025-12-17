39aadc3191023a66bcff8ebbb7256ab63b205e3c
x1.pub <x1_mailer@163.com>
2025-12-18 00:25:03 +0800

    Feature/split and reorganize menu layout (#6)
    * refactor: standardize component names and directory structure
    
    * refactor: split and reorganize menu layout

---
+++ client/src/components/menu-layout/components/redis-database/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { List } from 'antd';
+import classNames from 'classnames';
+
+import DatabaseIcon from '@/assets/svg/db.svg?react';
+import { RouterOutput } from '@/utils/trpc';
+import { generateActiveId } from '@/utils/use-main';
+import styles from './index.module.less';
+
+type TTableList = RouterOutput['table']['getList'];
+
+interface IRedisDatabaseProps {
+  database?: TTableList;
+  activeId?: string;
+  className?: string;
+  onClick?: (name: string) => void;
+}
+
+const RedisDatabase: React.FC<IRedisDatabaseProps> = ({
+  database,
+  activeId,
+  className,
+  onClick,
+}) => {
+  return (
+    <List
+      split={false}
+      dataSource={database}
+      className={className}
+      renderItem={item => (
+        <List.Item>
+          <span
+            className={classNames(styles.redis, {
+              [styles.active]: activeId === generateActiveId(item.name),
+            })}
+            onClick={() => onClick?.(item.name)}
+          >
+            <DatabaseIcon className={styles.databaseIcon} />
+            <span title={item.name}>{item.name}</span>
+          </span>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default RedisDatabase;
